d4021c0a81dfd6dba90553ec5d12d9dc8d402e3d
Aniket Kumar <aniketkumar693@gmail.com>
2022-03-14 13:24:28 +0530

    match count fixes

--- controllers/team-info.js
+++ controllers/team-info.js
@@ -51,7 +51,7 @@ const getSquadInfo = (req, res) => {
                 }
                 return res.send(response)
             }
-            else{
+            else {
                 res.send({ error: `Players not found` })
             }
         }
@@ -65,73 +65,72 @@ const getSquadInfo = (req, res) => {
 const getTeamFixtures = (req, res) => {
     var { teamId } = req.params
     var { uptoDate, matchesCount } = req.body
-    if (teamResults.length > 0) {
-        var teamDetails = teamResults.find(data => data.team_id == Number(teamId))
-        if (!teamDetails) {
-            res.send({ error: "No results found" })
-        }
-        else {
-            if (uptoDate || matchesCount) {
-                if (!uptoDate) {
-                    res.send({
-                        "error": "Please provide date"
-                    })
-                }
-                if (!matchesCount || matchesCount < 0) {
-                    matchesCount = 5
-                }
-                var fixtureQuery = `select * from fixturetable where fixture_date <= '${uptoDate}' and (teamA_id ='${teamId}' or teamB_id ='${teamId}') order by fixture_date desc LIMIT ${matchesCount};`
-            } else {
 
-
-                var fixtureQuery = `SELECT * FROM FixtureTable WHERE teamA_id=${teamDetails.team_id} OR teamB_id=${teamDetails.team_id}`
-            }
-            dbConnection.query(fixtureQuery, (err, result) => {
-                if (err) {
-                    res.json(responseMessage(response = err.message, isSuccess = false))
-                }
-                if (result.length > 0) {
-                    var response = []
-                    for (let index = 0; index < result.length; index++) {
-                        var fixtureDetails = result[index];
-                        var fixDate = new Date(Date.parse(fixtureDetails.fixture_date))
-                        if (teamDetails.team_id == fixtureDetails.teamA_id) {
-                            var opponent = teamResults.find(team => team.team_id == Number(fixtureDetails.teamB_id))
-
-                            response.push({
-                                "Club": teamDetails.team_name,
-                                "Opponents": opponent.team_name,
-                                "Venue": "Home",
-                                "Winner": fixtureDetails.winner,
-                                "Score": `${fixtureDetails.teamA_score} - ${fixtureDetails.teamB_score}`,
-                                Date: fixDate.toDateString()
-                            })
-                        }
-                        else {
-                            var opponent = teamResults.find(team => team.team_id == Number(fixtureDetails.teamA_id))
-
-                            response.push({
-                                "Club": teamDetails.team_name,
-                                "Opponents": opponent.team_name,
-                                "Venue": "Away",
-                                "Winner": fixtureDetails.winner,
-                                "Score": `${fixtureDetails.teamA_score} - ${fixtureDetails.teamB_score}`,
-                                Date: fixDate.toDateString()
-                            })
-                        }
-
-                    }
-                    return res.send(response)
-                }
-                res.send({ error: `Players not found for ${teamId}` })
+    if (uptoDate || matchesCount) {
+        if (!uptoDate) {
+            return res.send({
+                "error": "Please provide date"
             })
         }
+        if (!matchesCount || matchesCount < 0 || matchesCount > 10) {
+            if(matchesCount > 10){
+                return res.json({
+                    "error":"Only last 10 matches data can be obatined"
+                })
+            }
+            matchesCount = 5
+        }
+        var fixtureQuery = `SELECT * from teamtable;select * from fixturetable where fixture_date <= '${uptoDate}' and (teamA_id ='${teamId}' or teamB_id ='${teamId}') order by fixture_date desc LIMIT ${matchesCount};`
+    } else {
+        var fixtureQuery = `SELECT * from teamtable; SELECT * FROM FixtureTable WHERE teamA_id=${teamId} OR teamB_id=${teamId};`
     }
-    else {
+    dbConnection.query(fixtureQuery, (err, result) => {
+        if (err) {
+            return res.json(responseMessage(response = err.message, isSuccess = false))
+        }
+        try {
+            if (result.length > 0) {
+                var teamDetails = result[0].find(team => team.team_id == teamId)
+                var response = []
+                for (let index = 0; index < result[1].length; index++) {
+                    var fixtureDetails = result[1][index];
+                    var fixDate = new Date(Date.parse(fixtureDetails.fixture_date))
+                    if (teamDetails.team_id == fixtureDetails.teamA_id) {
+                        var opponent = result[0].find(team => team.team_id == Number(fixtureDetails.teamB_id))
 
-        res.send({ error: "Something went wrong" })
-    }
+                        response.push({
+                            "Club": teamDetails.team_name,
+                            "Opponents": opponent.team_name,
+                            "Venue": "Home",
+                            "Winner": fixtureDetails.winner,
+                            "Score": `${fixtureDetails.teamA_score} - ${fixtureDetails.teamB_score}`,
+                            Date: fixDate.toDateString()
+                        })
+                    }
+                    else {
+                        var opponent = result[0].find(team => team.team_id == Number(fixtureDetails.teamA_id))
+
+                        response.push({
+                            "Club": teamDetails.team_name,
+                            "Opponents": opponent.team_name,
+                            "Venue": "Away",
+                            "Winner": fixtureDetails.winner,
+                            "Score": `${fixtureDetails.teamA_score} - ${fixtureDetails.teamB_score}`,
+                            Date: fixDate.toDateString()
+                        })
+                    }
+
+                }
+                return res.send(response)
+            }
+            res.send({ error: `Players not found` })
+        } catch (error) {
+            console.log(error)
+            return res.send({ error: "Something went wrong" })
+        }
+
+    })
+
 }
-
 
 module.exports = { getTeamsList, getSquadInfo, getTeamFixtures }
